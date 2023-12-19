939bc78fb2433e34af6ccbb6f312a2e933d55ce9
Infostics <153814514+Infostics@users.noreply.github.com>
2023-12-19 21:25:38 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,7 @@
+def my_periodic_function():
+    print("Executing Python function...")
+
+# Example usage if run as a standalone Python script
+if __name__ == "__main__":
+
+    my_periodic_function()
